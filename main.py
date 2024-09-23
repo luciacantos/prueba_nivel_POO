ae33314f306c20186d7fb5084feb4fa18ae29693
luciacantos <luciacantos22@gmail.com>
2024-09-23 11:40:59 +0200

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+from subclases.bicicleta import bicicleta
+from subclases.coche import coche
+from subsubclases.motocicleta import motocicleta
+from subsubclases.camioneta import camioneta
+from lanzador import lanzador
+
+
+if __name__ == "__main__":
+    lanzador()
+
